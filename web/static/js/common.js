4fbf87bb8a006620fddf12111e6654f01424c8eb
lishaojie977 <1171987472@qq.com>
2020-04-22 17:36:44 +0800

    '登录模板，JS,后端controler'

---
+++ web/static/js/common.js
@@ -0,0 +1,17 @@
+;
+var common_ops = {
+    init:function(){
+        this.eventBind()
+    },
+    eventBind:function(){
+        console.log('common.js')
+    },
+    buildUrl:function(path,params){
+        var url = "" + path
+        return url
+    }
+}
+
+$(document).ready(function(){
+    common_ops.init();
+})
